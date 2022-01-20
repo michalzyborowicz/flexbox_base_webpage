f533ba27635874a0c6aa3a1e5e9656d3e155112d
Michal Zyborowicz <mt.zyborowicz@gmail.com>
2022-01-20 19:51:19 +0100

    website is finished all though i want to sit on nav bars problem (if i scroll on opened nav it changes color on black)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav')
 const burgerBtn = document.querySelector('.burger-btn')
 const navItem = document.querySelectorAll('.nav__item')
+const navBtnbars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
@@ -22,4 +25,21 @@ const handleNavItemsAnimation = () => {
 	})
 }
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+const handleObserver = () => {
+	const currentSection = window.scrollY
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnbars.classList.add('black-bars-color')
+		} else if(!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+			navBtnbars.classList.remove('black-bars-color')
+		}
+	})
+}
+window.addEventListener('scroll', handleObserver)
+handleCurrentYear()
 burgerBtn.addEventListener('click', handleNav)
